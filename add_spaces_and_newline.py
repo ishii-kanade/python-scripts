8153c80381cbfda75e6e02fec072ef1cb5278a71
kanade-ishii <iseekana@gmail.com>
2023-08-13 01:20:02 +0900

    gui機能を実装

--- add_spaces_and_newline.py
+++ add_spaces_and_newline.py
@@ -1,18 +1,39 @@
+import tkinter as tk
+from tkinter import filedialog
+
+
 def add_spaces_and_newline(text):
-    return text.replace('。', '。  \n')
+    return text.replace("。", "。  \n")
+
 
 def process_markdown_file(filepath):
-    # ファイルの内容を読み込む
-    with open(filepath, 'r', encoding='utf-8') as file:
+    with open(filepath, "r", encoding="utf-8") as file:
         content = file.read()
 
-    # 「。」の後に半角スペース２つと改行を挿入する処理を実行
     modified_content = add_spaces_and_newline(content)
 
-    # 結果を同じファイルに上書き保存
-    with open(filepath, 'w', encoding='utf-8') as file:
+    with open(filepath, "w", encoding="utf-8") as file:
         file.write(modified_content)
 
-# 使用例
-filepath = 'path_to_your_markdown_file.md'
-process_markdown_file(filepath)
+    result_label.config(text="変換が完了しました！")
+
+
+def open_file():
+    filepath = filedialog.askopenfilename(filetypes=[("Markdown Files", "*.md")])
+    if filepath:
+        process_markdown_file(filepath)
+
+
+app = tk.Tk()
+app.title("Markdown変換ツール")
+
+frame = tk.Frame(app)
+frame.pack(padx=10, pady=10)
+
+open_btn = tk.Button(frame, text="マークダウンファイルを選択", command=open_file)
+open_btn.pack(pady=20)
+
+result_label = tk.Label(frame, text="")
+result_label.pack(pady=20)
+
+app.mainloop()
